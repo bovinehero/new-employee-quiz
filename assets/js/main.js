d22c8c2049bdf1dc9496ca9b9ef457984c1ba9b3
Bovine Hero <garygrant79@yahoo.co.uk>
2022-11-20 20:55:20 +0000

    adding placeholder multiple guesses

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,33 @@ buildQuestions();
 
 // on submit, show results
 submitButton.addEventListener('click', showAnswers);
+
+const radioQuestions = [
+    {
+        question: "Question 1?",
+        answers: {
+        a: "radio option a",
+        b: "radio option b",
+        c: "radio option c",
+        },
+        specialConditionAnswer: "a"
+    },
+    {
+        question: "Question 2?",
+        answers: {
+            a: "radio option a",
+            b: "radio option b",
+            c: "radio option c",
+        },
+        specialConditionAnswer: "b"
+    },
+    {
+        question: "Question 3?",
+        answers: {
+            a: "radio option a",
+            b: "radio option b",
+            c: "radio option c",
+        },
+        specialConditionAnswer: "c"
+    },
+  ];
