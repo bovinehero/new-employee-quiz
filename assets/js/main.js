d7d5a3f2288e58e64853a5e327e77de04d3fd08d
Bovine Hero <garygrant79@yahoo.co.uk>
2022-11-20 20:46:24 +0000

    adding quiz ID selectors to JS code

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,7 @@
 * Main JavaScript for the site
 */
 console.log("JavaScript Connected!");
+
+let questionsContainer = document.getElementById('questions');
+let answersContainer = document.getElementById('answers');
+let submitButton = document.getElementById('submit');
