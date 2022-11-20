5a250ab7975a78874316a372ae774936753c4b14
Bovine Hero <garygrant79@yahoo.co.uk>
2022-11-20 20:50:00 +0000

    adding placeholder functions to build quiz and submit answers

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,17 @@ console.log("JavaScript Connected!");
 let questionsContainer = document.getElementById('questions');
 let answersContainer = document.getElementById('answers');
 let submitButton = document.getElementById('submit');
+
+function buildQuestions(){
+    console.log("Questions Built");
+}
+
+function showAnswers(){
+    console.log("Answers Displayed");
+}
+
+// display quiz right away
+buildQuestions();
+
+// on submit, show results
+submitButton.addEventListener('click', showAnswers);
